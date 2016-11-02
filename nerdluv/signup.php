28089d4802634f92e8c5cb945ca8e9165b6b3118
Josiah <josiah.parry@gmail.com>
2016-11-02 18:28:12 -0400

    class column

--- nerdluv/signup.php
+++ nerdluv/signup.php
@@ -9,21 +9,21 @@
           <input type="text" name="firstname" maxlength="16" size="16"><br>
         </li>
         <li>
-          <strong>Gender:</strong>
+          <strong class="column">Gender:</strong>
             <input id="male" type="radio" name="gender" value="male">
-            <label class="column" for="male">Male</label>
-            <label class="column" for="female">Female</label>
+            <label for="male">Male</label>
+            <label for="female">Female</label
               <input id="female" type="radio" name="gender" value="female"><br>
         </li>
         <li>
-          <strong>Personality types:</strong>
+          <strong class="column">Personality types:</strong>
           <input type="text" maxlength="4" size="4">
           <a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">
             Don't know your type?
           </a><br>
         </li>
         <li>
-          <strong>Favorite OS:</strong>
+          <strong class="column">Favorite OS:</strong>
           <select name="fav_os">
             <datalist>
               <option selected="selected">Windows</option>
@@ -35,7 +35,7 @@
           </select><br>
         </li>
         <li>
-          <strong>Seeking age:</strong>
+          <strong class="column">Seeking age:</strong>
             <input type="text" name="minage" placeholder="min" maxlength="2" size="6">
             to
             <input type="text" name="maxage" placeholder="max" maxlength="2" size="6"><br>
