47405083079e750ce91c085f24b6ae3adb4be0c0
Josiah <josiah.parry@gmail.com>
2016-11-02 17:45:12 -0400

    add ul

--- nerdluv/signup.php
+++ nerdluv/signup.php
@@ -3,18 +3,19 @@
 
     <fieldset>
         <legend>New User Signup</legend>
-    First name: <input type="text" name="firstname" maxlength="16" size="16"><br>
-    Gender: <label>
+    <ul>
+    <li>First name: <input type="text" name="firstname" maxlength="16" size="16"><br> </li>
+    <li>Gender: <label>
               <input type="radio" name="gender" value="male">
                 Male
-            </label>
+            </label><
             <label>
               <input type="radio" name="gender" value="female">
                 Female
-            </label><br>
-    Personality types: <input type="text" maxlength="4" size="4">
-      <a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a><br>
-    Favorite OS:
+            </label><br></li>
+    <li>Personality types: <input type="text" maxlength="4" size="4">
+      <a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a><br></li>
+    <li>Favorite OS:
       <select name="fav_os">
         <datalist>
         <option selected="selected">Windows</option>
@@ -23,11 +24,12 @@
         <option>Android</option>
         <option>iOS</option>
       </datalist>
-      </select><br>
-    Seeking age:
+    </select><br></li>
+    <li>Seeking age:
     <input type="text" name="minage" placeholder="min" maxlength="2" size="6"> to
     <input type="text" name="maxage" placeholder="max" maxlength="2" size="6"><br>
-    <input type="submit" name="submit"/>
+    <input type="submit" name="submit"/></li>
+  </ul>
   </fieldset>
   </form>
 
