ff2b2fc8d44080a6e53e87edca746c34323d890d
Josiah <josiah.parry@gmail.com>
2016-11-02 17:46:52 -0400

    add ul

--- nerdluv/signup.php
+++ nerdluv/signup.php
@@ -5,16 +5,16 @@
         <legend>New User Signup</legend>
     <ul>
     <li>First name: <input type="text" name="firstname" maxlength="16" size="16"><br> </li>
-    <li>Gender: <label>
-              <input type="radio" name="gender" value="male">
-                Male
-            </label><
-            <label>
-              <input type="radio" name="gender" value="female">
-                Female
-            </label><br></li>
+
+    <li>Gender:
+        <label><input type="radio" name="gender" value="male">Male
+        </label>
+        <label><input type="radio" name="gender" value="female">Female</label><br>
+    </li>
+
     <li>Personality types: <input type="text" maxlength="4" size="4">
       <a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a><br></li>
+
     <li>Favorite OS:
       <select name="fav_os">
         <datalist>
@@ -25,10 +25,12 @@
         <option>iOS</option>
       </datalist>
     </select><br></li>
+
     <li>Seeking age:
     <input type="text" name="minage" placeholder="min" maxlength="2" size="6"> to
     <input type="text" name="maxage" placeholder="max" maxlength="2" size="6"><br>
     <input type="submit" name="submit"/></li>
+    
   </ul>
   </fieldset>
   </form>
