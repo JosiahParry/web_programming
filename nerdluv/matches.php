3050345d807f54657a3483c6e1121fbab62e44f2
Josiah <josiah.parry@gmail.com>
2016-11-07 17:41:44 -0500

    match-sub

--- nerdluv/matches.php
+++ nerdluv/matches.php
@@ -5,13 +5,13 @@
       <legend>Returning User: </legend>
       <ul>
         <li>
-          <strong class="column">Name:</strong>
+          <strong class="column">Name:<strong>
           <input type=text id="name" name="name" size="16" maxlength="16"/>
         </li>
         <li>
           <br><input type="submit" value="View My Matches!">
         </li>
-      </ul>
+      <ul>
     </fieldset>
   </form>
 <?php include("bottom.html"); ?>
