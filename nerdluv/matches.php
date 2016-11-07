235f557ea22641aa8df19c0ac97b29821008d92b
Josiah <josiah.parry@gmail.com>
2016-11-06 19:20:09 -0500

    formatting matches

--- nerdluv/matches.php
+++ nerdluv/matches.php
@@ -5,8 +5,8 @@
       <legend>Returning User: </legend>
       <ul>
         <li>
-          <strong>Name:<strong>
-          <input type=text id="name" name="name" size="16"/><br>
+          <strong class="column">Name:<strong>
+          <input type=text id="name" name="name" size="16"/> <br>
           <input type="submit" value="View My Matches!">
         </li>
       <ul>
