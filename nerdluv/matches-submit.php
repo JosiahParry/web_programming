5348f4f5119671b5328a9f5ec11a59626861f690
Josiah <josiah.parry@gmail.com>
2016-11-07 17:35:39 -0500

    add basic matches-submit

---
+++ nerdluv/matches-submit.php
@@ -0,0 +1,6 @@
+<?php
+$name = _$GET["name"];
+$all_singles = fopen("singles.txt", "r");  //read only access to singles.txt
+$singles = explode("\n", $all_singles); //creates an array where each one is an individual
+print_r($singles);
+?>
