1559ff59f95a6ce930d6e7398374936f95a3711d
Josiah <josiah.parry@gmail.com>
2016-11-06 19:13:36 -0500

    add matches content

---
+++ nerdluv/matches.php
@@ -0,0 +1,10 @@
+
+<?php include("top.html"); ?>
+<form action="matches-submit.php" method="GET">
+    <fieldset>
+      <legend>Returning User: </legend>
+      <label for="name">Name:</label>
+      <input type=text id="name" name="name" size="16"/>
+      <input type="submit" value="View My Matches!">
+    </fieldset>
+<?php include("bottom.html"); ?>
