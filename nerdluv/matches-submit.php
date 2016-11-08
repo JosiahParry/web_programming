e8557ed97e0a3f6f42705ca39475960445b37c87
Josiah <josiah.parry@gmail.com>
2016-11-07 20:24:24 -0500

    stuff

--- nerdluv/matches-submit.php
+++ nerdluv/matches-submit.php
@@ -1,7 +1,24 @@
 <?php
 include("top.html");
-$all_singles = "singles.txt";  //read only access to singles.txt
-$singles = explode("\n", $all_singles); //creates an array where each one is an individual
+$all_singles = file("singles.txt");  //read only access to singles.txt
+//$singles = explode("\n", $all_singles); //creates an array where each one is an individual
 print_r($singles);
 include("bottom.html");
 ?>
+
+
+0 = Name
+1 = Gender
+2 = Age
+3 = Personality Type
+4 = OS
+5 = Min Age
+6 = Max Age
+
+<?php
+  foreach($all_singles as $key => $singles) {
+    $all_single[$key] = explode("\n");
+    print_r[$singles]
+  }
+
+ ?>
