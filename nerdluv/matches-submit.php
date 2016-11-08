851dc2d9ef482d009e00e40679d0d322a1b33327
Josiah <josiah.parry@gmail.com>
2016-11-07 22:31:24 -0500

    i hope this does something

--- nerdluv/matches-submit.php
+++ nerdluv/matches-submit.php
@@ -19,9 +19,14 @@ include("bottom.html");
   $singles = array();
   foreach($all_singles as $key => $ind) {
     $singles = explode("\n", $all_singles[$key]);
+
+  $person = fgets($all_singles);
+  $ppl_info[$key] = explode(",", $person);
   }
   $person = $_GET["name"];
   print_r($person);
   print_r($singles);
+
+  print_r($ppl_info);
 
  ?>
