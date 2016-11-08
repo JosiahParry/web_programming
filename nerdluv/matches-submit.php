bcab1b4da4db533e53b98db4a706da5783758076
Josiah <josiah.parry@gmail.com>
2016-11-07 20:40:19 -0500

    asa

--- nerdluv/matches-submit.php
+++ nerdluv/matches-submit.php
@@ -18,7 +18,7 @@ include("bottom.html");
 <?php
   foreach($all_singles as $key => $singles) {
     $singles = explode("\n", $all_singles[$key]);
-    echo "$singles <br>"
+    echo '$singles'.'<br>';
   }
   $person = $_GET["name"];
   print_r($person);
