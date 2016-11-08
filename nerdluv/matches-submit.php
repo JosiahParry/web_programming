c6dce0a4e240bf52d7e1d202dd5c276f6018a393
Josiah <josiah.parry@gmail.com>
2016-11-07 20:07:20 -0500

    test

--- nerdluv/matches-submit.php
+++ nerdluv/matches-submit.php
@@ -1,6 +1,6 @@
 <?php
 include("top.html");
-$all_singles = fopen("singles.txt", "r");  //read only access to singles.txt
+$all_singles = "singles.txt";  //read only access to singles.txt
 $singles = explode("\n", $all_singles); //creates an array where each one is an individual
 print_r($singles);
 include("bottom.html");
