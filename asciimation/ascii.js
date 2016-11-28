57a9458d3f59eb22f138668a778f52aeb612915a
Josiah <josiah.parry@gmail.com>
2016-11-28 15:51:47 -0500

    edit js

--- asciimation/ascii.js
+++ asciimation/ascii.js
@@ -1,7 +1,18 @@
 var font_sizes = ["7", "10", "12", "16", "24", "32"]
 
-function choose_animation() { //chooses which animation to display
-	var anime  = document.querySelector("#animation");
+body.onload function() {
+	document.getElementById("animation").onchange = choose_animation;
+	document.getElementById("start").onchange = start_animation;
+	document.getElementById("stop").onchange = stop_animation;
+}
+
+function choose_animation() {
+	var animation  = document.querySelector("#animation");
 	document.getElementById("viewer").value = ANIMATIONS[animation.value];
 	alert(ANIMATIONS[animation.value]);
+}
+
+function start() {
+	document.getElementById("start").disabled = false;
+	document.getElementbyId("stop").disabled = true;
 }
